881f67ea3507db9c2e77f33b4a89804444ae2f05
Raitis ZE <raitis.zgirskis@gmail.com>
2024-11-16 20:39:08 +0200

    Initial Commit

---
+++ AlphabetApp/AlphabetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphabetAppApp.swift
+//  AlphabetApp
+//
+//  Created by Raitis Zgirskis on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct AlphabetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
